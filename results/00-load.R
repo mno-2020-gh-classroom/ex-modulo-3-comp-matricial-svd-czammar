724ac0d02ef6d1a026286e2e8cfcd144545bf4de
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-04-16 00:46:02 -0500

    correcciones load y ejemplo test
    Signed-off-by: cesar zamora martinez <cesar_czm@ciencias.unam.mx>

--- results/00-load.R
+++ results/00-load.R
@@ -1,55 +1,123 @@
 ## 2.1 Algoritmo One-sided Jacobi para aproximar descomposicion SVD
 
 svd_jacobi_aprox <- function(A,TOL,maxsweep){
-  # Solucion del sistema A11 y = b1, usando algoritmo One-Sided Jacobi
-  svd <-svd_jacobi_aprox(bloq$A11,TOL,maxsweep)
-  U <-svd$U
-  S <- svd$S 
-  V<-svd$V
-  y = solver(U,S,V,bloq$b1)
+  # Calcula la descomposición de una matriz A en sus componentes U, S V, 
+  # utilizando el método de Jacobi para calcular la factorización SVD.De esta forma 
+  # la matriz A queda descompuesta de la siguiente forma: A = U*S*t(V).
+  # Args: 
+  #    A (matriz): Matriz de entrada (nxm) de números reales a la que se le calculará la descomposición SVD.
+  #    TOL (numeric): controla la convergencia del método, siendo un valor real de 10^-8 (sugerido en la nota 3.3.d.SVD)
+  #    Nota: Se sugiere una TOL mayor a 10^-32.
+  #    maxsweep (numeric): número máximo de sweeps,donde cada sweep consiste de un número máximo(nmax)
+  #    de rotaciones; y en cada sweep se ortogonalizan 2 columnas.
+  # Returns: 
+  #   Lista con 3 elementos, donde el primer elemento representa a la matriz S(mxm) matriz diagonal,el segundo a la matriz U(nxm)
+  #   y el tercero y último a la matriz V (mxm).En conjunto estas tres matrices componen la factorización SVD de la matriz de entrada A.
+  # Nota: Esta función estima la SVD thin,la cual calcula unicamente las m columnas de U correspondientes a los m renglones de V. De esta
+  # manera las columnas restantes de U no son calculadas, provocando una mejora significativa en velocidad de ejecución comparada con la 
+  # la Full SVD. Referencia: https://en.wikipedia.org/wiki/Singular_value_decomposition#Thin_SVD.
   
-  # Solucion del sistema A11 Y = A12 
-  Y= bloq$A12
+  #dimensiones
+  n<-dim(A)[2] #numero de columnas
+  m<-dim(A)[1] #numero de filas
+  nmax<-n*(n-1)/2
   
-  Y = solver(U,S,V,bloq$A12)
+  #inicializa valores del ciclo
+  ak<-A
+  vk<-diag(n)
+  sig <- NULL
+  uk <- ak
+  num_col_ortogonal<-0
+  k<-0
+  stop<-FALSE
   
-  # Construye matrix S = A22 A11^-1 A12
-  S = bloq$A22 - bloq$A21%*%Y
+  while(k<=maxsweep & num_col_ortogonal<nmax){
+    num_col_ortogonal<-0
+    for(i in 1:(n-1)){
+      for(j in (i+1):n){
+        col_j<-ak[,j]
+        col_i<-ak[,i]
+        
+        #comprueba ortogonalidad  
+        if(ortogonal(col_i,col_j,TOL)==1){
+          num_col_ortogonal<-num_col_ortogonal+1
+        }
+        else{
+          #calcula coeficientes de la matriz
+          a<-col_i%*%col_i
+          b<-col_j%*%col_j
+          c<-col_i%*%col_j
+          
+          #si c es cercano a cero no actualiza
+          if(c<TOL){
+            stop<-TRUE
+            break}
+          
+          #calcula la rotacion givens que diagonaliza
+          epsilon<-(b-a)/(2*c)
+          t<-signo(epsilon)/(abs(epsilon)+sqrt(1+epsilon**2))
+          cs<-1/sqrt(1+t**2)
+          sn<-cs*t
+          
+          #actualiza las columnas de la matriz ak
+          temp<-ak[,i] 
+          ak[,i]<-c(cs)*temp-c(sn)*ak[,j]
+          ak[,j]<-c(sn)*temp+c(cs)*ak[,j]
+          
+          
+          #actualiza las columnas de la matriz vk
+          temp<-vk[,i] #cambio
+          vk[,i]<-c(cs)*temp-c(sn)*vk[,j]
+          vk[,j]<-c(sn)*temp+c(cs)*vk[,j]             
+        }#cierra else
+      }#cierra for j
+      if(stop==TRUE){
+        stop<-FALSE
+        break
+      }
+    }#cierra for i
+    k<-k+1
+  }#cierra while
   
-  # Construye b_hat = b2 - A21 A11^-1 b1
-  b_hat = bloq$b2-bloq$A21%*%y
+  #Obtener sigma
+  sig<-apply(ak, 2, function(x){norm(x,"2")})
   
-  # Solucion del sistema S x2 = b_hat algoritmo One-Sided Jacobi
-  x_2 = sel_solver(S,b_hat,TOL, maxsweep)
+  #Obtener U
+  for(i in 1:n){
+    if (sig[i]<TOL){
+      uk[,i]<-0  
+    } else{
+      uk[,i] <- ak[,i]/sig[i]
+    }
+  }
   
-  # Construye b_hat2 S x2 = b1 - A12 b_hat
-  b_hat2 = bloq$b1 -bloq$A12%*%x_2
+  # Indices de sigma ordenada en forma decreciente para ordenar V,S,U
+  index <- order(sig,decreasing = TRUE)
+  vk <- vk[,index]
+  S <- diag(sig[index])
+  uk <- uk[,index]
   
-  # Solucion del sistema A11 x2 = b_hat algoritmo One-Sided Jacobi
-  x_1 = solver(U,S,V,b_hat2)
-  
-  # Solucion del sistema original
-  x <- c(x_1,x_2)
-  x
+  list(S = S, U = uk, V= vk)
 }
 
-## 2.2 Solver de sistemas lineales usando aproximacion SVD
+## 2.2 Linear solver aproximating SVD decomposition using One-sided Jacobi algorithm
+
 sel_solver<-function(A,b,TOL=10**-8,maxsweep=20){
-  # Aproxima la solución de un sistema de ecuaciones lineales (SEL) de la forma Ax=b 
-  # usando la descomposición SVD de A obtenido por medio del método de One-sided Jacobi.
-  # Args: 
-  #    A (matriz): matriz de coeficientes del SEL, de números reales y de dimension m x n.
-  #    b (vector): vector de lado derecho del sistema, de números reales y de dimension m.
-  #    TOL (numeric): real positivo, que sirve para controlar la convergencia del metodo iterativo. 
-  #             Nota: se usa un valor default de 10^-8 como tolerancia
-  #    maxsweep (int): numero entero posiitivo, que se usar para controlar el número máximo de sweeps
-  #                    que se ejecutan como parte del metodo iterativo.
-  # Returns: 
-  #    x (vector): vector solución del SEL, con dimension n.
-  
-  svd<-svd_jacobi_aprox(A,TOL,maxsweep)
-  x<-solver(svd$U,svd$S,svd$V,b)
-  x
+    # Aproxima la solución de un sistema de ecuaciones lineales (SEL) de la forma Ax=b 
+    # usando la descomposición SVD de A obtenido por medio del método de One-sided Jacobi.
+    # Args: 
+    #    A (matriz): matriz de coeficientes del SEL, de números reales y de dimension m x n.
+    #    b (vector): vector de lado derecho del sistema, de números reales y de dimension m.
+    #    TOL (numeric): real positivo, que sirve para controlar la convergencia del metodo iterativo. 
+    #             Nota: se usa un valor default de 10^-8 como tolerancia
+    #    maxsweep (int): numero entero posiitivo, que se usar para controlar el número máximo de sweeps
+    #                    que se ejecutan como parte del metodo iterativo.
+    # Returns: 
+    #    x (vector): vector solución del SEL, con dimension n.
+
+    svd<-svd_jacobi_aprox(A,TOL,maxsweep)
+    x<-solver(svd$U,svd$S,svd$V,b)
+    x
 }
 
 ##  2.3 Eliminación por bloques basada en solver linear que usa SVD
